76b6d410812c61b3a6a7e98a2e95a1f5f194ec19
Igor Shentsov <ishencow@gmail.com>
2021-04-06 23:37:50 +0300

    Моковые данные

---
+++ lib/mocks.dart
@@ -0,0 +1,29 @@
+import "domain/sight.dart";
+
+final List<Sight> mocks = 
+[
+  Sight(
+    "test1",
+    0,
+    1,
+    "https://URL1",
+    "detail1",
+    "type1",
+  ),
+  Sight(
+    "test2",
+    1,
+    0,
+    "https://URL2",
+    "detail2",
+    "type2",
+  ),
+  Sight(
+    "test3",
+    1,
+    1,
+    "https://URL3",
+    "detail3",
+    "type3",
+  ),
+];
