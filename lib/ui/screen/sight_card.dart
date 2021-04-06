a8b529d1d7bf5acd87e8358d8792559547533138
Igor Shentsov <ishencow@gmail.com>
2021-04-06 23:54:50 +0300

    SightCard

---
+++ lib/ui/screen/sight_card.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import "../../domain/sight.dart";
+
+class SightCard extends StatelessWidget {
+  Sight _sight;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+  SightCard(this._sight);
+}
