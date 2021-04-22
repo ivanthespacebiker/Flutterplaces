7a229efefa93b6f07def2186633ba679d359fa31
Igor Shentsov <ishencow@gmail.com>
2021-04-22 19:02:48 +0300

    Верстка экрана "Хочу посетить/Посещенные места"

--- lib/ui/style/picturesAssets.dart
+++ lib/ui/style/picturesAssets.dart
@@ -22,3 +22,11 @@ const String SightDetailsFavorite = "res/pictures/SightDetailsFavorite.png";
 const String ListVizitedEmptyBackground = "res/pictures/ListVizitedEmptyBackground.png";
 
 const String ListWantToVizitEmptyBackground = "res/pictures/ListWantToVizitEmptyBackground.png";
+
+const String BNB_Heart = "res/pictures/BNB_Heart.png";
+
+const String BNB_List = "res/pictures/BNB_List.png";
+
+const String BNB_Map = "res/pictures/BNB_Map.png";
+
+const String BNB_Settings = "res/pictures/BNB_Settings.png";
