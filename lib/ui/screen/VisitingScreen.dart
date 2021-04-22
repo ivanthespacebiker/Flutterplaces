048949487f40ba3c4c33c09b75fdc9127ac86f22
Igor Shentsov <ishencow@gmail.com>
2021-04-22 16:08:29 +0300

    VisitingScreen

---
+++ lib/ui/screen/VisitingScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+/// Экран "Хочу посетить/Посещенные места"
+class VisitingScreen extends StatefulWidget {
+  @override
+  _VisitingScreenState createState() => _VisitingScreenState();
+}
+
+class _VisitingScreenState extends State<VisitingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
